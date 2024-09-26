97f7cd353f061b5069036b22f8c4ea837ba1f63e
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-09-26 14:02:33 +0700

    fix: l

--- apps/blinks/constants.ts
+++ apps/blinks/constants.ts
@@ -1,5 +1,4 @@
 import { Connection } from '@solana/web3.js'
 
 export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL as string
-export const connection = new Connection(process.env.HELIUS_RPC as string, 'confirmed')
-export const TILE_SIZE = 64
+export const connection = new Connection('https://api.devnet.solana.com', 'confirmed')
