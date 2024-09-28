1477364ad538d42993add7d62f106a56a33687ee
sonhavietnamese <nguyensonha.hanz@gmail.com>
2024-09-28 22:07:19 +0700

    fix: add deps

--- apps/blinks/utils/create-deal-damage-tx.ts
+++ apps/blinks/utils/create-deal-damage-tx.ts
@@ -1,6 +1,6 @@
 import { BOSS_PUBLIC_KEY, connection, PROGRAM } from '@/constants'
+import { BN } from '@coral-xyz/anchor'
 import { PublicKey, SystemProgram } from '@solana/web3.js'
-import BN from 'bn.js'
 
 export const createDealDamageTransaction = async (sender: PublicKey, damage: number) => {
   const tx = await PROGRAM.methods
